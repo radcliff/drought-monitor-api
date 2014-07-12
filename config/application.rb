d678719ef5deb44985546478768315d19a8e876d
Sterling Radcliff <radcliff@riseup.net>
2014-07-12 14:31:49 -0700

    configured active record post his adapter (*database.yml overwritten by Heroku on build)

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+require "active_record/connection_adapters/postgis_adapter/railtie" # PostGIS adapter for Active Record
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
