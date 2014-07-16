0fa1e6dad3ad7f53cb3d0e767adfd1263e9e7922
Sterling Radcliff <radcliff@riseup.net>
2014-07-15 21:19:50 -0700

    requests for state drought data willl render a topojson response

---
+++ app/controllers/droughtshapes_controller.rb
@@ -0,0 +1,18 @@
+class DroughtshapesController < ApplicationController
+  include ApplicationHelper
+
+  def index
+    if params[:s]
+      state_name = params[:s].split.map {|i| i.capitalize}.join(" ")
+      state = State.find_by(name: state_name)
+      if state
+        shapes = state.drought_shapes
+        geojson_shapes = to_geojson(shapes)
+
+        render json: convert_to_topojson(geojson_shapes)
+      else
+        render json: "not found"
+      end
+    end
+  end
+end
