26ab80a1e0c8ebf1f56b5682a7bffeba0cf41ade
Sterling Radcliff <radcliff@riseup.net>
2014-07-15 21:18:12 -0700

    further modularized helpers: application helper => geojson helper + topojson helper; added topojson gem

---
+++ app/helpers/geojson_helper.rb
@@ -0,0 +1,26 @@
+module GeojsonHelper
+
+  # this method takes in a RGeo geometry object and returns a GeoJSON object
+  def to_geojson(geometry_collection)
+    
+    geojson_object = {}
+    geojson_object["type"] = "FeatureCollection"
+    geojson_object["features"] = []
+
+    geometry_collection.each_with_index do |record, index|
+    feature = RGeo::GeoJSON::Feature.new(record.shape, index, {:properties => { :dm => record.dm } })
+      geojson_object["features"].push(RGeo::GeoJSON.encode(feature))
+    end
+
+    return geojson_object
+  end  
+
+  # this method takes in a GeoJSON object and saves it to disk
+  def json_to_file(object, file_path_and_name)
+    open("#{file_path_and_name}.json", "wb") do |file|  
+      file.write(object.to_json)
+      true
+    end
+  end
+
+end
