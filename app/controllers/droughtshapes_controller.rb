efc081a2cc46fbed9e7244cd60f2978c28fae1f2
Sterling Radcliff <radcliff@riseup.net>
2014-07-16 12:27:42 -0700

    added support for :geo parameter, if "true" response will be in geojson

--- app/controllers/droughtshapes_controller.rb
+++ app/controllers/droughtshapes_controller.rb
@@ -8,10 +8,13 @@ class DroughtshapesController < ApplicationController
       if state
         shapes = state.drought_shapes
         geojson_shapes = to_geojson(shapes)
-
-        render json: convert_to_topojson(geojson_shapes)
+        if params[:geo] == "true"
+          render json: geojson_shapes
+        else
+          render json: convert_to_topojson(geojson_shapes)
+        end
       else
-        render json: "not found"
+        render json: "not found - check query string and try again"
       end
     end
   end
