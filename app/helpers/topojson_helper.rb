befc86cfc271bd4d14fa3cf935da9399b03e13eb
Sterling Radcliff <radcliff@riseup.net>
2014-07-25 08:38:47 -0700

    when geojson created record.dm is set to "id" => when topojson created property does not need promotion

--- app/helpers/topojson_helper.rb
+++ app/helpers/topojson_helper.rb
@@ -5,8 +5,9 @@ module TopojsonHelper
 
     source = open('./lib/assets/topojson.js').read
     context = ExecJS.compile(source)
-
+    
     topojson = context.eval("topojson.topology({collection: #{collection}})")
+
     return topojson
   end
   
